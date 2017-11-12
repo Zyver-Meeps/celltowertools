4577e0cf1e57f62eb4708bb0672576b2c08a3bbe
Enzo Gladiadis <31501617+iLuvLinux@users.noreply.github.com>
2017-11-12 20:43:54 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+    /**
+     * Calculate the bearing between two positions as a value from 0-360
+     *
+     * @param lat1 - The latitude of the first position
+     * @param lng1 - The longitude of the first position
+     * @param lat2 - The latitude of the second position
+     * @param lng2 - The longitude of the second position
+     *
+     * @return int - The bearing between 0 and 360
+     */
+    bearing : function (lat1,lng1,lat2,lng2) {
+        var dLon = (lng2-lng1);
+        var y = Math.sin(dLon) * Math.cos(lat2);
+        var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+        var brng = this._toDeg(Math.atan2(y, x));
+        return 360 - ((brng + 360) % 360);
+    },
+
+   /**
+     * Since not all browsers implement this we have our own utility that will
+     * convert from degrees into radians
+     *
+     * @param deg - The degrees to be converted into radians
+     * @return radians
+     */
+    _toRad : function(deg) {
+         return deg * Math.PI / 180;
+    },
+
+    /**
+     * Since not all browsers implement this we have our own utility that will
+     * convert from radians into degrees
+     *
+     * @param rad - The radians to be converted into degrees
+     * @return degrees
+     */
+    _toDeg : function(rad) {
+        return rad * 180 / Math.PI;
+    },
